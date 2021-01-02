30b7c78bb143289b230d720f05844bf4b384f82a
Shohid <68284522+Mshohid@users.noreply.github.com>
2021-01-02 12:51:11 +0000

    Add files via upload

---
+++ advanced-backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ # This bash script is used to backup a user's home directory to /tmp/
+
+ user=$(whoami)
+ input=/home/$user
+ output=/tmp/${user}_home_$(date +%A).tar.gz
+ 
+ tar -czvf $output $input 2> /dev/null 
+ echo "Backup of $input completed! Details about the output backup file:"
+ ls -l $output 
+
